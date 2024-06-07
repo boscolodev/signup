381f15c31cd9e5d936acdb8641af02373b14500e
BoscoloDev <boscolo.dev@gmail.com>
2024-06-07 08:13:52 -0300

    Initial Project

---
+++ src/main/java/br/com/gbs/singup/SingupApplication.java
@@ -0,0 +1,13 @@
+package br.com.gbs.singup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/gbs/singup/SingupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gbs.singup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
